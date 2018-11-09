325753d08c1941166dffe0a5a453bb9fa8ec0976
Adwait Bhave <adwait.bv@gmail.com>
2018-11-09 18:05:07 +0530

    completed first 4 strategies

--- Phase1-StockPrediction/vasu_strategy.py
+++ Phase1-StockPrediction/vasu_strategy.py
@@ -34,3 +34,87 @@ def detect_bearish_continuation(train_data,test_data):
                 recco_dict['entry_date'] = signal_data.index.to_pydatetime()[0]
             
     return recco_dict
+
+def detect_bearish_reversal(train_data,test_data):
+    recco_dict = {}
+    medium_term_trend = vasu.get_medium_trend(train_data)
+    
+    peak_info =  vasu.get_latest_peak_info(train_data)
+    peak_type = peak_info['peak_type']
+    peak_data = peak_info['peak_data']
+    
+    peak_date = peak_data.index.to_pydatetime()[0]
+    peak_low_slope = peak_data['low_slope'].values[0]
+   
+    if medium_term_trend != 'bullish' or medium_term_trend != 'indeterminate':
+        if peak_type == 'low' and peak_low_slope <= 0.0:
+            recco_dict['medium_trend'] = medium_term_trend
+            recco_dict['latest_peak'] = peak_type
+            recco_dict['peak_slope'] = peak_low_slope
+            recco_dict['trade'] = 'buy'
+            recco_dict['benchmark_date'] = peak_date
+            benchmark_price =  peak_data['High Price'].values[0]
+            
+            recco_dict['benchmark_price'] = benchmark_price
+            
+            signal_data = test_data[test_data['Close Price'] > benchmark_price][1:2].copy()
+            if len(signal_data) > 0:
+                recco_dict['entry_date'] = signal_data.index.to_pydatetime()[0]
+            
+    return recco_dict
+
+def detect_bullish_reversal(train_data,test_data):
+    recco_dict = {}
+    medium_term_trend = vasu.get_medium_trend(train_data)
+    
+    peak_info =  vasu.get_latest_peak_info(train_data)
+    peak_type = peak_info['peak_type']
+    peak_data = peak_info['peak_data']
+    
+    peak_date = peak_data.index.to_pydatetime()[0]
+    peak_high_slope = peak_data['high_slope'].values[0]
+   
+    if medium_term_trend != 'bearish' or medium_term_trend != 'indeterminate':
+        if peak_type == 'high' and peak_high_slope >= 0.0:
+            recco_dict['medium_trend'] = medium_term_trend
+            recco_dict['latest_peak'] = peak_type
+            recco_dict['peak_slope'] = peak_high_slope
+            recco_dict['trade'] = 'sell'
+            recco_dict['benchmark_date'] = peak_date
+            benchmark_price =  peak_data['Low Price'].values[0]
+            
+            recco_dict['benchmark_price'] = benchmark_price
+            
+            signal_data = test_data[test_data['Close Price'] < benchmark_price][1:2].copy()
+            if len(signal_data) > 0:
+                recco_dict['entry_date'] = signal_data.index.to_pydatetime()[0]
+                
+    return recco_dict
+
+def detect_bullish_continuation(train_data,test_data):
+    recco_dict = {}
+    medium_term_trend = vasu.get_medium_trend(train_data)
+    
+    peak_info =  vasu.get_latest_peak_info(train_data)
+    peak_type = peak_info['peak_type']
+    peak_data = peak_info['peak_data']
+    
+    peak_date = peak_data.index.to_pydatetime()[0]
+    peak_low_slope = peak_data['low_slope'].values[0]
+   
+    if medium_term_trend != 'bearish' or medium_term_trend != 'indeterminate':
+        if peak_type == 'low' and peak_low_slope <= 0.0:
+            recco_dict['medium_trend'] = medium_term_trend
+            recco_dict['latest_peak'] = peak_type
+            recco_dict['peak_slope'] = peak_low_slope
+            recco_dict['trade'] = 'buy'
+            recco_dict['benchmark_date'] = peak_date
+            benchmark_price =  peak_data['High Price'].values[0]
+            
+            recco_dict['benchmark_price'] = benchmark_price
+            
+            signal_data = test_data[test_data['Close Price'] > benchmark_price][:1].copy()
+            if len(signal_data) > 0:
+                recco_dict['entry_date'] = signal_data.index.to_pydatetime()[0]
+            
+    return recco_dict
